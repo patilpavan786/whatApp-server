ef57c5277144340725e358c7a33d809134f254a1
Pavan Patil <119596213+patilpavan786@users.noreply.github.com>
2023-05-16 12:35:39 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+var mongoose = require("mongoose");
+var dotenv = require("dotenv");
 
 dotenv.config();
-const USERNAME = process.env.DB_USERNAME;
-const PASSWORD = process.env.DB_PASSWORD;
+var USERNAME = process.env.DB_USERNAME;
+var PASSWORD = process.env.DB_PASSWORD;
 
-const Connection = async function() {
-    const URL = `mongodb://${USERNAME}:${PASSWORD}@ac-kx7kxom-shard-00-00.y3pd8zs.mongodb.net:27017,ac-kx7kxom-shard-00-01.y3pd8zs.mongodb.net:27017,ac-kx7kxom-shard-00-02.y3pd8zs.mongodb.net:27017/?ssl=true&replicaSet=atlas-5shhdg-shard-0&authSource=admin&retryWrites=true&w=majority`;
+var Connection = async function() {
+    var URL = "mongodb://" + USERNAME + ":" + PASSWORD + "@ac-kx7kxom-shard-00-00.y3pd8zs.mongodb.net:27017,ac-kx7kxom-shard-00-01.y3pd8zs.mongodb.net:27017,ac-kx7kxom-shard-00-02.y3pd8zs.mongodb.net:27017/?ssl=true&replicaSet=atlas-5shhdg-shard-0&authSource=admin&retryWrites=true&w=majority";
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true });
         console.log("Database connected successfully ");
     } catch (error) {
-        console.log(`error while connecting the database  ${error.message}`);
+        console.log("error while connecting the database " + error.message);
     }
 };
 
