481a84a16eac46b9866a5a75b22d4f5c70410e5b
Pavan Patil <119596213+patilpavan786@users.noreply.github.com>
2023-05-16 12:43:16 +0530

    Update upload.js

--- utils/upload.js
+++ utils/upload.js
@@ -1,28 +1,26 @@
-import multer from "multer";
-import { GridFsStorage } from 'multer-gridfs-storage';
-
-
-import dotenv from 'dotenv';
+var multer = require('multer');
+var GridFsStorage = require('multer-gridfs-storage');
+var dotenv = require('dotenv');
 
 dotenv.config();
 
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
+var username = process.env.DB_USERNAME;
+var password = process.env.DB_PASSWORD;
 
-const storage = new GridFsStorage({
-    url: `mongodb://${username}:${password}@ac-kx7kxom-shard-00-00.y3pd8zs.mongodb.net:27017,ac-kx7kxom-shard-00-01.y3pd8zs.mongodb.net:27017,ac-kx7kxom-shard-00-02.y3pd8zs.mongodb.net:27017/?ssl=true&replicaSet=atlas-5shhdg-shard-0&authSource=admin&retryWrites=true&w=majority`,
+var storage = new GridFsStorage({
+    url: 'mongodb://' + username + ':' + password + '@ac-kx7kxom-shard-00-00.y3pd8zs.mongodb.net:27017,ac-kx7kxom-shard-00-01.y3pd8zs.mongodb.net:27017,ac-kx7kxom-shard-00-02.y3pd8zs.mongodb.net:27017/?ssl=true&replicaSet=atlas-5shhdg-shard-0&authSource=admin&retryWrites=true&w=majority',
     options: { useNewUrlParser: true },
-    file: (request, file) => {
-        const match = ["image/png", "image/jpg"];
+    file: function(request, file) {
+        var match = ['image/png', 'image/jpg'];
 
-        if(match.indexOf(file.memeType) === -1) 
-            return`${Date.now()}-blog-${file.originalname}`;
+        if (match.indexOf(file.memeType) === -1)
+            return Date.now() + '-blog-' + file.originalname;
 
         return {
-            bucketName: "photos",
-            filename: `${Date.now()}-blog-${file.originalname}`
-        }
+            bucketName: 'photos',
+            filename: Date.now() + '-blog-' + file.originalname
+        };
     }
 });
 
-export default multer({storage}); 
+module.exports = multer({ storage: storage });
