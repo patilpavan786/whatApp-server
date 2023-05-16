2bf825700ec57902fce388ebbe01213cb3116ff2
Pavan Patil <119596213+patilpavan786@users.noreply.github.com>
2023-05-16 11:54:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import Connection from "./database/db.js"
-import Route from "./routes/route.js";
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const Connection = require("./database/db.js");
+const Route = require("./routes/route.js");
 
 const app = express();
-app.use(cors()) ;
+app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/' , Route);
-
+app.use('/', Route);
 
 Connection();
-const PORT  = 8000;
+const PORT = 8000;
 
-app.listen(PORT,()=> console.log(`server is running succesfuly on PORT ${PORT}`))
-
-
+app.listen(PORT, () => console.log(`server is running successfully on PORT ${PORT}`));
